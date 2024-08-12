85b84c00f38df09dfbee355150d34b06ab49066e
jtorreggiani <joetorreggiani@gmail.com>
2024-08-11 17:36:41 -0700

    Start refactoring communities and resources.

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,51 +1,19 @@
-import { redirect } from 'next/navigation';
-import { getResources } from '@/app/utils/getResources';
-import Link from 'next/link';
-import ResourcesSection from '../components/ResourcesSection';
-import ResourcesSidebar from '../components/ResourcesSideBar';
+import InfoSection from '@/app/components/InfoSection';
+import Resources from '@/app/components/Resources';
+import ContactResidentServices from '@/app/components/ContactResidentServices';
 
 export async function generateStaticParams() {
-  const categories = await getResources();
+  const community_paths = ['arroyo-village', 'garden-court'];
 
-  const paths = categories
-    .filter((category) => category.slug !== undefined)
-    .map((category) => ({ slug: category.slug }));
-  return paths;
+  return community_paths.map((slug) => ({ slug }));
 }
 
-export default async function CategoriesPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const categories = await getResources();
-  const category = categories.find((category) => category.slug === params.slug);
-
-  if (!category) {
-    redirect('/');
-  }
-
+export default function CommunityPage() {
   return (
-    <main className='text-white '>
-      <Link href='/' className='hover:underline'>
-        Back to Resources
-      </Link>
-      <section className='flex flex-row py-5'>
-        <div id='sidebar' className='basis-5/12'>
-          <h1 className='text-2xl font-bold'>{category.name}</h1>
-          <ResourcesSidebar resources={category.resources} />
-        </div>
-        <div className='w-full'>
-          {category.resources.map((resource) => (
-            <div key={resource.name}>
-              <ResourcesSection
-                title={resource.name}
-                resources={resource.data}
-              />
-            </div>
-          ))}
-        </div>
-      </section>
+    <main className='space-y-6'>
+      <InfoSection />
+      <Resources />
+      <ContactResidentServices />
     </main>
   );
 }
--- content/infoSection.ts
+++ content/infoSection.ts
@@ -1,11 +1,12 @@
 export const FeatureHighlightContent = {
-  imagePath: '/placeholder.png',
+  imagePath: '/resident-resources-website/placeholder.png',
   imageAlt: 'Computer Lab Now Open',
   // description:
   //   'Join us in the computer lab, in the Community Building Room 2. Check for hours.',
   featureTitle: 'Arroyo Village’s Computer Lab NOW OPEN!',
   mobileFeatureTitle: 'Computer Lab NOW OPEN!',
-  mobileDescription: 'Arroyo Village’s computer lab is now open. Wednesdays/Thursdays 5:30pm-7:30pm'
+  mobileDescription:
+    'Arroyo Village’s computer lab is now open. Wednesdays/Thursdays 5:30pm-7:30pm',
 };
 
 export const scheduleItems = [
