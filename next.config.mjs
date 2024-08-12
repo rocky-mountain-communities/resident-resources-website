7f986a84924bcbc4a4cbd4eef9f463ed79061722
jtorreggiani <joetorreggiani@gmail.com>
2024-08-11 17:07:35 -0700

    Test full path.

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,14 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
+  output: 'export',
 
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "",
+  basePath: '/resident-resources-website',
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
